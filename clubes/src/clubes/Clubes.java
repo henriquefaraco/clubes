d832e67f0c737a457b8b33ef6e840363b4a3054d
Joao <jlborgh@gmail.com>
2018-06-15 20:54:28 -0300

    enviando arquivos

---
+++ clubes/src/clubes/Clubes.java
@@ -0,0 +1,21 @@
+
+package clubes;
+
+import clubes.model.Conexao;
+import clubes.view.TelaPrincipal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Clubes {
+
+    public static void main(String[] args) throws SQLException {
+    
+       Connection conexao = Conexao.conecta();
+       
+        TelaPrincipal abertura = new TelaPrincipal();
+        abertura.setVisible(true);
+        
+    }
+    
+}
